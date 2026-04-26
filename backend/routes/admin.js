6f1c95171042c7b0a0e0af9df9d5dd82045ef097
NAKAAYI SHAMIRAH <shamienakaayi@gmail.com>
2026-04-26 02:01:04 -0700

    Fix all User import paths

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const User = require("../models/User");
+const User = require("../models/user");
 
 router.get("/users", async (req, res) => {
   const users = await User.findAll();
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const User = require("../models/User");
+const User = require("../models/user");
 
 router.post("/subscribe", async (req, res) => {
   const { userId, plan, subjects } = req.body;
