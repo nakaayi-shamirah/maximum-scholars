7f04b0289d8120010945b789d4655a9b9bc1711a
NAKAAYI SHAMIRAH <shamienakaayi@gmail.com>
2026-04-26 02:04:24 -0700

    Fix all User import paths

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const User = require("../models/ufser");
+const User = require("../models/user");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
