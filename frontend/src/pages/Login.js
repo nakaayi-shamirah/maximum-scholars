0219bc9adfb08eeae02b5671ff13a38dbdf41c74
NAKAAYI SHAMIRAH <shamienakaayi@gmail.com>
2026-04-28 11:22:05 -0700

    Fix login API URL

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -20,7 +20,7 @@ export default function Login() {
     useState("");
 
   const API =
-    "https://maximum-scholars.onrender.com";
+    "https://maximum-scholars-1-api.onrender.com";
 
   const handleLogin =
     async (e) => {
